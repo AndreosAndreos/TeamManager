3ee111c7bb0baecea14f694afdeb30f89d24ca21
Andrzej Chwalenia <andrzejchwalenia@gmail.com>
2025-01-12 17:08:01 +0100

    1st commit

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -13,6 +13,9 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 
+using System.Configuration;
+using System.Data.SqlClient;
+
 namespace TeamManager
 {
     /// <summary>
@@ -20,9 +23,61 @@ namespace TeamManager
     /// </summary>
     public partial class MainWindow : Window
     {
+        SqlConnection con;
+
         public MainWindow()
         {
             InitializeComponent();
+            DataBaseConnect();
+            
+
         }
+
+        private void TxtBoxTeam_GotFocus(object sender, RoutedEventArgs e)
+        {
+            if (TxtBoxTeam.Text == "Enter team name")
+            {
+                TxtBoxTeam.Text = "";
+                TxtBoxTeam.Foreground = Brushes.Black;
+            }
+        }
+
+        private void TxtBoxTeam_LostFocus(object sender, RoutedEventArgs e)
+        {
+            if (string.IsNullOrWhiteSpace(TxtBoxTeam.Text))
+            {
+                TxtBoxTeam.Text = "Enter team name";
+                TxtBoxTeam.Foreground = Brushes.Gray;
+            }
+        }
+
+        public static void DataBaseConnect()
+        {
+            string connectionString = ConfigurationManager.ConnectionStrings["TeamManager.Properties.Settings.TeamManagerConnectionString"].ConnectionString;
+
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+                try
+                {
+                    connection.Open();
+                    MessageBox.Show("The connection to database has been established.");
+
+                }
+                catch (SqlException sqlex) 
+                {
+                    MessageBox.Show($"There's been a problem with establishing the database connection {sqlex.Message}");
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show($"Error: {ex.Message}");
+                }
+            }
+        }
+
+        private void BtnTeam_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
     }
 }
