37f34c5bf86d714e05b2c5469e00cd5776052c14
Andrzej Chwalenia <andrzejchwalenia@gmail.com>
2025-01-12 19:03:35 +0100

    commit

---
+++ Properties/AssemblyInfo.cs
@@ -0,0 +1,23 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Informacje ogólne o zestawie
+[assembly: AssemblyTitle("TeamManager")]
+[assembly: AssemblyDescription("Opis Twojej aplikacji")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Twoja Firma")]
+[assembly: AssemblyProduct("TeamManager Produkt")]
+[assembly: AssemblyCopyright("Copyright © Twoja Firma 2025")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Widoczność dla COM
+[assembly: ComVisible(false)]
+
+// GUID dla identyfikacji w przypadku widoczności w COM
+[assembly: Guid("12345678-abcd-1234-ef00-1234567890ab")]
+
+// Wersja zestawu
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
