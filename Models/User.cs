2edb2b840ede5486cf6e838171d92a5ee871b20e
Andrzej Chwalenia <andrzejchwalenia@gmail.com>
2025-01-15 11:34:28 +0100

    made the windows grid more responsive

--- Models/User.cs
+++ Models/User.cs
@@ -7,13 +7,13 @@ using System.Threading.Tasks;
 namespace TeamManager.Models
 {
     /// <summary>
-    /// A class representing a database user with diferent privileges: Admin, User, Analyst
+    /// A class representing a database user with diferent privileges: Admin = 1, User = 2, Analyst = 3
     /// </summary>
     public class User
     {
         public int Id { get; set; }
         public string UserName { get; set; }
         public string PasswordHash { get; set; }
-        public string Role { get; set; }
+        public int Role { get; set; }
     }
 }
