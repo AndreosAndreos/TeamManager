4679afd7a6bff959810f15451074caa4313fe947
Andrzej Chwalenia <andrzejchwalenia@gmail.com>
2025-01-15 12:48:27 +0100

    commit

---
+++ LoginAndRegistation.xaml.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace TeamManager
+{
+    /// <summary>
+    /// Interaction logic for LoginAndRegistation.xaml
+    /// </summary>
+    public partial class LoginAndRegistation : Window
+    {
+        private string connectionString = ConfigurationManager.ConnectionStrings["TeamManager.Properties.Settings.TeamManagerConnectionString"].ConnectionString;
+
+        DataClasses1DataContext dataContext;
+        public LoginAndRegistation()
+        {
+            InitializeComponent();
+
+            dataContext = new DataClasses1DataContext(connectionString);
+        }
+        public void DataBaseConnect()
+        {
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+                try
+                {
+                    connection.Open();
+                }
+                catch (SqlException sqlex)
+                {
+                    MessageBox.Show($"There's been a problem with establishing the database connection {sqlex.Message}");
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show($"Error: {ex.Message}", "Error", MessageBoxButton.OK, MessageBoxImage.Warning);
+                }
+            }
+        }
+
+        private void BtnEnter_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void BtnRegister_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
